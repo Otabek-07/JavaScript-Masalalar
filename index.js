07bd076a2d30ab452f703be27b890d0ede76fd7d
otabek <otajongold@gmail.com>
2024-03-29 13:45:27 +0500

    Js masala

---
+++ index.js
@@ -0,0 +1,65 @@
+/* 14 - Masala
+let son = +prompt("Uch xonali Sonni kiriting");
+let brinchi = son%10;
+let ikkinchi = (son%100-brinchi)/10;
+let uchunchi = (son-son%100)/100;
+alert(`${brinchi} ${uchunchi} ${ikkinchi}`);*/
+
+
+/* 15 - Masala
+ let son=+prompt("Uch xonali Sonni kiriting: ");
+ let uch=son%10; 
+ let ikki=(son%100-son%10)/10; 
+ let bir=(son-son%100)/100;
+ alert(`${ikki} ${bir} ${uch}`);
+
+
+ 16 - Masala
+ let son=+prompt("Uch xonali Sonni kiriting: ");
+ let uch=son%10; 
+ let ikki=(son%100-son%10)/10; 
+ let bir=(son-son%100)/100;
+ alert(`${bir} ${uch} ${ikki}`);
+
+ 17 - Masala
+ let son=+prompt("Sonni kiriting: ");
+ let yuzlar=(son%1000-son%100)/100;
+ alert(`${yuzlar}`)
+
+  18 - Masala
+  let son=+prompt("Sonni kiriting: ");
+  let yuzlar=(son%10000-son%1000)/1000;
+  alert(`${yuzlar}`)
+
+
+   19 - Masala
+   let N=+prompt("Sekundlarni kiriting: ");
+   let qoldiq=N%60;
+   let natija=(N-qoldiq)/60;
+   alert(`${natija}`);
+
+   20 - Masala
+   let N=+prompt("Sekundlarni kiriting: ");
+   let qoldiq=N%3600;
+   let natija=(N-qoldiq)/3600;
+   alert(`${natija}`);
+
+
+   21 - Masala
+   let N=+prompt("Sekundlarni kiriting: ");
+   let qoldiq=N%60;
+   let natija=(N-qoldiq)/60;
+   alert(`${natija} Minut ${qoldiq} Sekunt`);
+
+    22 - Masala
+   let N=+prompt("Sekundlarni kiriting: ");
+   let qoldiq=N%3600;
+   let natija=(N-qoldiq)/3600;
+   alert(`${natija} Soat ${qoldiq} Sekund utgan`);
+
+   23 - Masala
+   let N=+prompt("Sekundlarni kiriting: ");
+   let soat=(N-N%3600)/3600;
+   let minut=(N%3600-N%60)/60;
+   let sekund=N%60;
+   alert(`${soat} Soat ${minut} Minut ${sekund} Sekund utgan`);*/
